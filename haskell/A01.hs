3bc2708ff16e5c018fea12781d96f7e64630138d
Lttce <31960436+Lttce@users.noreply.github.com>
2024-12-25 21:22:58 +0900

    update

--- haskell/A01.hs
+++ haskell/A01.hs
@@ -1,1 +1,9 @@
 -- A01 - The First Problem
+-- https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
+main :: IO()
+main = do
+    n <- readLn :: IO Int
+    print $ solve n
+
+solve :: Int -> Int
+solve x = x * x
