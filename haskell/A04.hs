d5c835f425c171805f2dcc256b3167e49e5c0cf5
Lttce <31960436+Lttce@users.noreply.github.com>
2025-01-04 18:53:03 +0900

    Create A04.hs

---
+++ haskell/A04.hs
@@ -0,0 +1,20 @@
+-- A04 - Binary Representation 1
+-- https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_d
+
+main :: IO()
+main = do
+    n <- readLn :: IO Int
+    putStrLn $ solve n
+
+solve :: Int -> String
+solve x = pad ++ bin
+    where
+        bin = toBin x
+        pad = replicate (10 - length bin) '0'
+
+toBin :: Int -> String
+toBin 0 = ""
+toBin x = reverse $ toBin' x
+    where
+        toBin' 0 = ""
+        toBin' y = show (mod y 2) ++ toBin' (div y 2)
